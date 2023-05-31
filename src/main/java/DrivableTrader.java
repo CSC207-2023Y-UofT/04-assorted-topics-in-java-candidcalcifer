2a538307ce45327d510dd4e2809fe9fd27514855
chocolatecakes <serena.he.03@gmail.com>
2023-05-31 18:20:15 -0400

    05/31 pt2

--- src/main/java/DrivableTrader.java
+++ src/main/java/DrivableTrader.java
@@ -10,6 +10,7 @@
  * Look at DomesticatableTrader.java for an example.
  */
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DrivableTrader extends Trader<Drivable>{
@@ -24,12 +25,20 @@ public class DrivableTrader extends Trader<Drivable>{
     public DrivableTrader(List<Drivable> inventory, List<Drivable> wishlist, int money) {
         super(inventory, wishlist, money);
     }
+
+    public DrivableTrader(int money) {
+        super(money);
+    }
+
     @Override
-    public int getSellingPrice(Drivable obj){
-        if (obj instanceof Tradable){
-            return ((Tradable) obj).getPrice() + obj.getMaxSpeed();
-        } else {
-            return Tradable.MISSING_PRICE;
+    public int getSellingPrice(Drivable item) {
+        int super_price = super.getSellingPrice(item);
+        int item_speed = item.getMaxSpeed();
+        if (super_price == Tradable.MISSING_PRICE) {
+            return super_price;
         }
+
+        return super_price + item_speed;
+
     }
 }
