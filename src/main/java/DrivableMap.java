a0aa5ef3470fba3724e86d2da3e097b061cdceac
chocolatecakes <serena.he.03@gmail.com>
2023-05-31 18:11:05 -0400

    05/31

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -10,6 +10,7 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class DrivableMap {
     HashMap<String, Drivable> drivable_map;
@@ -27,9 +28,14 @@ class DrivableMap {
      *       in drivable_map, then add the pair to drivable_map.
      *       Return true if the Drivable was added to drivable_map.
      */
-
-
-
+    public boolean addDrivable(String id, Drivable obj){
+        if (!drivable_map.containsKey(id)){
+            drivable_map.put(id, obj);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     /* TODO: Write a method named hasFasterThan that takes an int (a speed)
      *       and returns true iff there is at least one item in drivable_map
@@ -37,15 +43,28 @@ class DrivableMap {
      * You may want to use drivable_map.keys() or drivable_map.values() to
      * iterate through drivable_map.
      */
-
-
-
-
+    public boolean hasFasterThan(int speed){
+        for (Drivable value : drivable_map.values()) {
+            if ((value.getMaxSpeed() - speed >= 0)){
+                return true;
+            }
+        }
+        return false;
+    }
 
     /* TODO: Write a method named getTradable that takes no arguments and
      *       returns a List containing all of the Tradable items in
      *       drivable_map.
      */
+    public ArrayList<Tradable> getTradable(){
+        ArrayList<Tradable> trade = new ArrayList<Tradable>();
+        for (Drivable value : drivable_map.values()) {
+            if (value instanceof Tradable){
+                trade.add((Tradable) value);
+            }
+        }
+        return trade;
+    }
 
 
 
--- src/main/java/DrivableTrader.java
+++ src/main/java/DrivableTrader.java
@@ -11,3 +11,25 @@
  */
 
 import java.util.List;
+
+public class DrivableTrader extends Trader<Drivable>{
+    /**
+     * DrivableTrader, giving them the given inventory,
+     * wishlist, and money.
+     *
+     * @param inventory Objects in this Trader's inventory
+     * @param wishlist  Objects in this Trader's wishlist
+     * @param money     The Trader's money
+     */
+    public DrivableTrader(List<Drivable> inventory, List<Drivable> wishlist, int money) {
+        super(inventory, wishlist, money);
+    }
+    @Override
+    public int getSellingPrice(Drivable obj){
+        if (obj instanceof Tradable){
+            return ((Tradable) obj).getPrice() + obj.getMaxSpeed();
+        } else {
+            return Tradable.MISSING_PRICE;
+        }
+    }
+}
